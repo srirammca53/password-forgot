5a3673ec4acbdb727f5c55c9e93c4a68fba22497
sriram <sriram.in20@gmail.com>
2011-07-28 11:33:49 +0530

    11th commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Heroku::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-config.action_mailer.default_url_options = { :host => 'http://thridapp.heroku.com/' }
+config.action_mailer.default_url_options = { :host => 'thridapp.heroku.com/' }
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
